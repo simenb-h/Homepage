e94ed76afab3fd070af253c967a512b51cb005c0
simenb-h <38004714+simenb-h@users.noreply.github.com>
2021-03-21 19:50:20 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
-import Aboutme from "./components/Aboutme";
+//import Aboutme from "./components/Aboutme";
 import Project from "./components/Project";
 
 function App() {

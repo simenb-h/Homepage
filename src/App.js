44dd1afd7af42be222c715bd46a2b23ccf8dbe9d
simenb-h <38004714+simenb-h@users.noreply.github.com>
2021-03-21 19:53:47 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
-//import Aboutme from "./components/Aboutme";
 import Project from "./components/Project";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        //<Aboutme/>
         <Project/>
       </header>
     </div>

c919550fd6bbb54addd41967d473925361fde59b
simenb-h <38004714+simenb-h@users.noreply.github.com>
2021-03-21 19:51:38 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Aboutme/>
+        //<Aboutme/>
         <Project/>
       </header>
     </div>
